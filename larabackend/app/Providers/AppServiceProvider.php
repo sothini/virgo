85366bd48cfb77ac56653d18091c20444b9cf97d
sothini <kaongallostam@gmail.com>
2025-12-26 00:53:22 +0200

    fix:add cors and inject dependencies via service provider

--- larabackend/app/Providers/AppServiceProvider.php
+++ larabackend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Interfaces\IAuthRepository;
+use App\Repositories\AuthRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(IAuthRepository::class, AuthRepository::class);
     }
 
     /**
