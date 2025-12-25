3cf9ed534c3937a2c7f712c5712604646ad43365
sothini <kaongallostam@gmail.com>
2025-12-26 00:12:54 +0200

    feature: installed swagger documentation for api

--- larabackend/app/Http/Controllers/Controller.php
+++ larabackend/app/Http/Controllers/Controller.php
@@ -2,6 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(
+    version: "1.0.0",
+    title: "Laravel API Documentation",
+    description: "API documentation for the Laravel application",
+    contact: new OA\Contact(
+        email: "kaongallostan@gmail.com"
+    ),
+    license: new OA\License(
+        name: "Apache 2.0",
+        url: "http://www.apache.org/licenses/LICENSE-2.0.html"
+    )
+)]
+#[OA\PathItem(path: "/api")]
 abstract class Controller
 {
     //
